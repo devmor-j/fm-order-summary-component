0e7182ec00c8f08d2af0c8e8fd6ec3becbe105b6
Morteza Jamshidi <devmor.j@gmail.com>
2022-04-02 16:02:54 +0430

    Add colors

--- windi.config.js
+++ windi.config.js
@@ -10,7 +10,14 @@ export default defineConfig({
     extend: {
       fontFamily: {
         RedHatDisplay: ['Red Hat Display', 'sans-serif'],
-      }
-    }
+      },
+      colors: {
+        'clr-pale-blue': 'hsl(225, 100%, 94%)', // Pale blue
+        'clr-bright-blue': 'hsl(245, 75%, 52%)', // Bright blue
+        'clr-very-pale-blue': 'hsl(225, 100%, 98%)', // Very pale blue
+        'clr-desat-blue': 'hsl(224, 23%, 55%)', // Desaturated blue
+        'clr-dark-blue': 'hsl(223, 47%, 23%)', // Dark blue
+      },
+    },
   }
 });
